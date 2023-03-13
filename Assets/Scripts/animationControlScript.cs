e614bbecb14a7536565d77e5b86b465f85894bc2
Jhonnatan Barbosa <69597805+jhonnatanbarbosa@users.noreply.github.com>
2023-03-13 09:05:18 -0300

    Update Test

--- Assets/Scripts/animationControlScript.cs
+++ Assets/Scripts/animationControlScript.cs
@@ -4,86 +4,54 @@ using UnityEngine;
 
 public class animationControlScript : MonoBehaviour
 {
-
     Animator animator;
     public rigidBodyMovement rbM;
-   
-
+    
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        #region ifs
         if (Input.GetKey("d") || Input.GetKey("a")){
 
-            animator.SetBool("isRunning(wo/ shifht)", true);
-        }
+            animator.SetBool("isRunning(wo/ shifht)", true);}
+        else{animator.SetBool("isRunning(wo/ shifht)", false);}
 
-        if (!Input.GetKey("d") && !Input.GetKey("a")){
-
-            animator.SetBool("isRunning(wo/ shifht)", false);
-        }
-        
         if (Input.GetKey(KeyCode.LeftShift) && (Input.GetKey("d") || Input.GetKey("a"))){
 
-            animator.SetBool("isRunning", true);
-        }
-
-        if (!Input.GetKey(KeyCode.LeftShift)){
-
-            animator.SetBool("isRunning", false);
-        }
+            animator.SetBool("isRunning", true);}
+        else{animator.SetBool("isRunning", false);}
 
         if (Input.GetKey(KeyCode.Space) && (Input.GetKey(KeyCode.LeftShift) || ((Input.GetKey("d") || Input.GetKey("a"))))){
 
-            animator.SetBool("isJumping", true);
-        }
-
-        if (!Input.GetKey(KeyCode.Space)){
-
-            animator.SetBool("isJumping", false);
-        }
+            animator.SetBool("isJumping", true);}
+        else{animator.SetBool("isJumping", false);}
 
         if (Input.GetKey(KeyCode.Space)){
-            animator.SetBool("idleJumping", true);
-        }
+            animator.SetBool("idleJumping", true);}
 
-        if (!Input.GetKey(KeyCode.Space)){
-            animator.SetBool("idleJumping", false);
-        }
+        else{animator.SetBool("idleJumping", false);}
 
         //check if player is in the air
         if(rbM.isGrounded == true)
         {
-            animator.SetBool("isGroundeded", true);
-        }
-        if(rbM.isGrounded == false)
-        {
-            animator.SetBool("isGroundeded", false);
-        }
+            animator.SetBool("isGroundeded", true);}
+        else{animator.SetBool("isGroundeded", false);}
 
         if(rbM.atWallL || rbM.atWallR){
             
-            animator.SetBool("atWall", true);
-        }
+            animator.SetBool("atWall", true);}
+        else{animator.SetBool("atWall", false);}
 
-        if(!rbM.atWallL && !rbM.atWallR){
-            
-            animator.SetBool("atWall", false);
-        }
+        if (Input.GetKey(KeyCode.LeftControl) && (Input.GetKey("d") || Input.GetKey("a"))){
 
-         if (Input.GetKey(KeyCode.LeftControl) && (Input.GetKey("d") || Input.GetKey("a"))){
-
-            animator.SetBool("isSliding", true);
-        }
-
-        if (!Input.GetKey(KeyCode.LeftControl)){
-
-            animator.SetBool("isSliding", false);}
-
+            animator.SetBool("isSliding", true);}
+        else{animator.SetBool("isSliding", false);}
+        #endregion
     }
 }
