b0f957441e4797cdec92278c0225c14922721e21
Jhonnatan Barbosa <69597805+jhonnatanbarbosa@users.noreply.github.com>
2023-03-29 17:40:33 -0300

    Update v1.27
    - New Animations 
       - New Dash Punch(Vi Q)
       - New Slide
       - New Punch UP while jumping (Shoriuken)
    
    - New optimized animations scripts and parameters
    
    - New Cooldown System for the Slide
    
    - Adjusted Sun and Moon Rotations on the Preview Map
    
    - Organized, Renamed and Removed files.

--- Assets/Scripts/animationControlScript.cs
+++ Assets/Scripts/animationControlScript.cs
@@ -4,128 +4,152 @@ using UnityEngine;
 
 public class animationControlScript : MonoBehaviour
 {
-    Animator animator;
-    public rigidBodyMovement rbM;
-    int randomNumber;
+    Animator animator; // reference to the Animator component attached to the same GameObject
+    public rigidBodyMovement rbM; // reference to the rigidBodyMovement script attached to another GameObject
+    int randomNumber; // variable to hold a random integer value
 
-    private bool isFightingBool;
-        
-    // Start is called before the first frame update
+    private float airTime; // float variable to hold the time the character is in the air
+
+    public bool isFightingBool; // boolean variable to check if the character is fighting
+    public bool isPunchingBool; // boolean variable to check if the character is punching
+    
     void Start()
     {
-        animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>(); // get the Animator component on this GameObject
+        airTime = 0f; // set the 'airTime' float variable to 0f
     }
     
-    // Update is called once per frame
     void Update()
     {
-
-        randomNumber = Random.Range(0, 10);
+        randomNumber = Random.Range(0, 10); // generate a random integer between 0 and 9
         
-        #region ifs
-        if (Input.GetKey("d") || Input.GetKey("a")){
 
-            animator.SetBool("isRunning(wo/ shifht)", true);}
-        else{animator.SetBool("isRunning(wo/ shifht)", false);}
 
-        if (Input.GetKey(KeyCode.LeftShift) && (Input.GetKey("d") || Input.GetKey("a"))){
+        // set animation states based on user input
+        // set the 'isRunning(wo/ shifht)' boolean parameter to true if the user is pressing the 'd' or 'a' keys
+        animator.SetBool("isRunning(wo/ shifht)", Input.GetKey("d") || Input.GetKey("a")); 
+        
+        // set the 'isRunning' boolean parameter to true if the user is pressing the 'd' or 'a' keys and the 'LeftShift' key
+        animator.SetBool("isRunning", Input.GetKey(KeyCode.LeftShift) && (Input.GetKey("d") || Input.GetKey("a"))); 
 
-            animator.SetBool("isRunning", true);}
-        else{animator.SetBool("isRunning", false);}
+        // set the 'isJumping' boolean parameter to true if the user is pressing the 'Space' key and the 'LeftShift' key or the 'd' or 'a' keys
+        animator.SetBool("isJumping", Input.GetKey(KeyCode.Space) && (Input.GetKey(KeyCode.LeftShift) || ((Input.GetKey("d") || Input.GetKey("a"))))); 
 
-        if (Input.GetKey(KeyCode.Space) && (Input.GetKey(KeyCode.LeftShift) || ((Input.GetKey("d") || Input.GetKey("a"))))){
+        // set the 'idleJumping' boolean parameter to true if the user is pressing the 'Space' key
+        animator.SetBool("idleJumping", Input.GetKey(KeyCode.Space));
 
-            animator.SetBool("isJumping", true);}
-        else{animator.SetBool("isJumping", false);}
+        // set the 'isGrounded' boolean parameter to true if the character is grounded
+        animator.SetBool("isGroundeded", rbM.isGrounded);
 
-        if (Input.GetKey(KeyCode.Space)){
-            animator.SetBool("idleJumping", true);}
+        // set the 'atWall' boolean parameter to true if the character is at a wall
+        animator.SetBool("atWall", rbM.atWallL || rbM.atWallR);
 
-        else{animator.SetBool("idleJumping", false);}
+        // set the 'isSliding' boolean parameter to true if the user is pressing the 'LeftControl' key and the 'd' or 'a' keys
+        animator.SetBool("isSliding", Input.GetKey(KeyCode.LeftControl) && rbM.slideCooldown <= 0f && (Input.GetKey("d") || Input.GetKey("a")));
 
-        //check if player is in the air
-        if(rbM.isGrounded == true)
-        {
-            animator.SetBool("isGroundeded", true);}
-        else{animator.SetBool("isGroundeded", false);}
-
-        if(rbM.atWallL || rbM.atWallR){
-            
-            animator.SetBool("atWall", true);}
-        else{animator.SetBool("atWall", false);}
-
-        if (Input.GetKey(KeyCode.LeftControl) && (Input.GetKey("d") || Input.GetKey("a"))){
-
-            animator.SetBool("isSliding", true);}
-        else{animator.SetBool("isSliding", false);}
-
-        if (Input.GetKey("f")){
-
-            animator.SetBool("isFighting", true);
-            rbM.fight = 0.15f;
-            isFightingBool = true;}
-
-        if (Input.GetKey("g")){
-
-            animator.SetBool("isFighting", false);
-            rbM.fight = 1f;
-            isFightingBool=false;}
-
-        if (Input.GetKey("x")){
-
-            int variation = Random.Range(0, 3);
-            rbM.fight = 0f;
-
-            switch (variation)
-            {
-                case 0:
-                animator.SetInteger("punchVariation", variation);
-                animator.SetBool("isPunching", true);
-                break;
-
-                case 1:
-                animator.SetInteger("punchVariation", variation);
-                animator.SetBool("isPunching", true);
-                break;
-
-                case 2:
-                animator.SetInteger("punchVariation", variation);
-                animator.SetBool("isPunching", true);
-                break;
-            }
-            
-        }else{animator.SetBool("isPunching", false);}
-
-        if (Input.GetKey("c")){
-            
-            int variation = Random.Range(0, 3);
-
-            switch (variation)
-            {
-                case 0:
-                animator.SetInteger("kickVariation", variation);
-                animator.SetBool("isKicking", true);
-                break;
-
-                case 1:
-                animator.SetInteger("kickVariation", variation);
-                animator.SetBool("isKicking", true);
-                break;
-
-                case 2:
-                animator.SetInteger("kickVariation", variation);
-                animator.SetBool("isKicking", true);
-                break;
-            }
-        }else{animator.SetBool("isKicking", false);}
-
-        if(isFightingBool == true)
-        {
-            rbM.fight = 0.15f;
-            animator.SetInteger("isFightingIdle", randomNumber);
-            Debug.Log(randomNumber);
+        // if the character is in the air, add the time since the last frame to the 'airTime' float variable
+        if (rbM.isGrounded == false){ 
+            airTime += Time.deltaTime; 
+            animator.SetFloat("airTime", airTime);
         }
-        else{rbM.fight = 1f;}
+        else{
+            airTime = 0f; // if the character is grounded, set the 'airTime' float variable to 0f
+        }
+
+
+
+
+
+
+        #region Vi Punching
+        // if the character is fighting, punching and in the air, and the user is pressing the 'a' key, set the 'isPunching' boolean parameter to true and add a force to the character
+        if(isFightingBool == true && isPunchingBool == true && rbM.isGrounded == false && Input.GetKey("a")){ 
+
+            animator.SetBool("isPunching", true);
+            rbM.rb.AddForce(Vector3.left * 30f, ForceMode.Impulse);
+        }
+        else{
+            animator.SetBool("isPunching", false); //set the 'isPunching' boolean parameter to false
+        }
+
+        // if the character is fighting, punching and in the air, and the user is pressing the 'd' key, set the 'isPunching' boolean parameter to true and add a force to the character
+        if(isFightingBool == true && isPunchingBool == true && rbM.isGrounded == false && Input.GetKey("d")){
+
+            animator.SetBool("isPunching", true); // set the 'isPunching' boolean parameter to true
+            rbM.rb.AddForce(Vector3.right * 30f, ForceMode.Impulse); // add a force to the character
+        }
+        else{
+            animator.SetBool("isPunching", false); //set the 'isPunching' boolean parameter to false
+        }
         #endregion
+
+
+
+
+
+         // if the character is in the air and punches, set the 'shoriuken' boolean parameter to true
+        if(rbM.isGrounded == false && isFightingBool == true && isPunchingBool == true){
+
+            animator.SetBool("shoriuken", true); // set the 'shoriuken' boolean parameter to true
+            rbM.rb.AddForce(Vector3.up * 20f, ForceMode.Impulse); // add a force to the character
+        }
+        
+        if(rbM.isGrounded == true){
+
+            animator.SetBool("shoriuken", false); // set the 'shoriuken' boolean parameter to false
+        }
+
+
+
+
+
+        // handle character fighting animations and actions
+
+        if (Input.GetKeyDown("f"))
+        {
+            animator.SetBool("isFighting", true); // set the 'isFighting' boolean parameter to true in the Animator
+            rbM.fight = 0.15f; // set the 'fight' variable in the rigidBodyMovement script to 0.15f
+            isFightingBool = true; // set the 'isFightingBool' boolean variable to true
+        }
+        else if (Input.GetKeyDown("g"))
+        {
+            animator.SetBool("isFighting", false); // set the 'isFighting' boolean parameter to false in the Animator
+            rbM.fight = 1f; // set the 'fight' variable in the rigidBodyMovement script to 1f
+            isFightingBool = false; // set the 'isFightingBool' boolean variable to false
+        }
+        else if (Input.GetKeyDown("x"))
+        {
+            int variation = Random.Range(0, 3); // generate a random integer between 0 and 2
+            rbM.fight = 0f; // set the 'fight' variable in the rigidBodyMovement script to 0f
+            animator.SetInteger("punchVariation", variation); // set the 'punchVariation' integer parameter in the Animator to the generated value
+            animator.SetBool("isPunching", true); // set the 'isPunching' boolean parameter in the Animator to true
+            isPunchingBool = true; // set the 'isPunchingBool' boolean variable to true
+        }
+        else
+        {
+            animator.SetBool("isPunching", false); // set the 'isPunching' boolean parameter in the Animator to false
+            isPunchingBool = false; // set the 'isPunchingBool' boolean variable to false
+        }
+
+        if (Input.GetKeyDown("c"))
+        {
+            int variation = Random.Range(0, 3); // generate a random integer between 0 and 2
+            animator.SetInteger("kickVariation", variation); // set the 'kickVariation' integer parameter in the Animator to the generated value
+            animator.SetBool("isKicking", true); // set the 'isKicking' boolean parameter in the Animator to true
+        }
+        else
+        {
+            animator.SetBool("isKicking", false); // set the 'isKicking' boolean parameter in the Animator to false
+        }
+
+        if (isFightingBool) // check if character is fighting
+        {
+            rbM.fight = 0.15f; // if the character is fighting, lower its movement speed
+            animator.SetInteger("isFightingIdle", randomNumber); // set the 'isFightingIdle' integer parameter in the Animator to the generated random integer
+        }
+        else
+        {
+            rbM.fight = 1f; // if the character is not fighting, set its movement speed to normal
+        }
     }
 }
