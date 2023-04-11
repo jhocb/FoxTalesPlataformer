1958d1336449d48dff9ed0ade588943866f58e11
Jhonnatan Barbosa <69597805+jhonnatanbarbosa@users.noreply.github.com>
2023-04-11 11:57:35 -0300

    Update v1.36

--- Assets/slideHitDetection.cs
+++ Assets/slideHitDetection.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class slideHitDetection : MonoBehaviour
 {
-    public float damage = 50f;
+    public float damage = 50f; // damage dealt to enemy
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy")) // if the object that enters the trigger is tagged as an enemy
         {
-            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>();
-            enemyHealth.curHealth -= damage;
+            EnemyHealth enemyHealth = other.gameObject.GetComponent<EnemyHealth>(); //  get enemy health script
+            enemyHealth.curHealth -= damage; // subtract damage from enemy health
         }
     }
 }
