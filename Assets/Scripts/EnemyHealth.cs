098e510dd579294de2f92196cb7e14e5ef94734f
Jhonnatan Barbosa <69597805+jhonnatanbarbosa@users.noreply.github.com>
2023-04-26 08:01:52 -0300

    Update v1,41,2

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,30 @@
+using UnityEngine; 
+using System.Collections; 
+  
+public class EnemyHealth : MonoBehaviour 
+{ 
+    public float maxHealth = 100f; // this is the max health of the enemy 
+    public float curHealth; // this is the current health of the enemy 
+ 
+    Animator animator; // this is the animator component of the enemy 
+  
+    void Start() { 
+ 
+        curHealth = maxHealth; // set the current health to the max health 
+        animator = GetComponent<Animator>(); // get the Animator component of the enemy 
+    } 
+ 
+    // Update is called once per frame 
+    void Update() 
+    { 
+        if (curHealth > maxHealth) // if the current health is greater than the max health 
+        { 
+            curHealth = maxHealth; // set the current health to the max health 
+        } 
+        if (curHealth <= 0) // if the current health is less than or equal to 0 
+        { 
+            //Destroy(gameObject); // destroy the enemy 
+            animator.SetBool("gotHitted", true); 
+        } 
+    }
+}    
---
+++ Assets/Scripts/MoveTo.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform player; // Player object
+    public Transform goal; // Goal object
+    public Transform goal2; // Goal object
+
+    private float time; // Time variable
+    private float punchTime; // Time variable
+
+    private NavMeshAgent agent; // NavMeshAgent component
+    Animator animator; // this is the animator component of the enemy 
+
+    public float distance = 100f; // Distance variable
+    public float maxDistance = 4f; // Max distance variable
+    public float minDistance = 0.85f; // Min distance variable
+    public float refreshRate = 0.5f; // Refresh rate variable
+    public float punchDistance = 0.95f; // Punch distance variable
+    public float punchCooldown = 1f; // Punch cooldown variable
+    
+
+    // Start is called before the first frame update
+    void Start(){
+
+        time = 0; // Initialize time to 0
+        punchTime = 0; // Initialize time to 0
+        agent = GetComponent<NavMeshAgent>(); // Get NavMeshAgent component
+        animator = GetComponent<Animator>(); // get the Animator component of the enemy 
+    }
+
+    void FixedUpdate(){
+
+        time += Time.deltaTime; // Increment time by the time between frames
+        punchTime += Time.deltaTime; // Increment time by the time between frames
+
+        distance = Vector3.Distance(transform.position, player.transform.position); // Calculate distance between player and goal
+
+        if (time >= refreshRate && (distance <=maxDistance || distance >= minDistance)){ // If time is greater than 0.5 seconds and the distance is less than 10 and greater than 0.5
+            goal.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z); // Set goal position to player position
+            agent.destination = goal.position; // Set the destination to the goal position
+            time = 0f; // Reset time
+            animator.SetBool("running", true); // Set running to true
+        } 
+
+        if(distance > maxDistance || distance < minDistance){ // if the distance is greater than 7 and less than 0.7
+            agent.destination = transform.position; // Set the destination to the goal position
+            animator.SetBool("running", false); // Set running to false
+        }
+
+        if(distance <= punchDistance && punchTime >= punchCooldown){ // if the distance is less than 0.7
+            animator.SetBool("punching", true); // Set punching to true
+            punchTime = 0f; // Reset punch time
+            animator.SetBool("punching", false); // Set running to false
+        }
+    }
+}
