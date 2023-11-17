fefa6566fbd3fa25555429f9466510a4068fa9e6
Hanadadev <spacezcanal@gmail.com>
2023-11-16 22:05:07 -0300

    Update v.1.04.1
    Tower Parkour:
    - Changed platform positions and colliders to fit the new controllers
    - Tested (personal) parkour segmenet
    
    Shaders:
    - Added "See Through" shader which allows to see through objects in the layer "Wall" and have the shader pipeline activated.

---
+++ Assets/Scripts/circleSync.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleSync : MonoBehaviour
+{
+    public static int PosID = Shader.PropertyToID("_Position");
+    public static int SizeID = Shader.PropertyToID("_Size");
+
+    public Material WallMaterial;
+    public Camera Camera;
+    public LayerMask Mask;
+
+
+    void Update()
+    {
+        var dir = Camera.transform.position - transform.position;
+        var ray = new Ray(transform.position, dir.normalized);
+
+        if (Physics.Raycast(ray, 3000, Mask))
+            WallMaterial.SetFloat(SizeID, 1); 
+        else
+            WallMaterial.SetFloat(SizeID, 0);
+            
+        var view = Camera.WorldToViewportPoint(transform.position);
+        WallMaterial.SetVector(PosID, view);
+    }
+}
